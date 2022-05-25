d2307e1926c6915518a8e9798e388e3babf80e3b
Yossapol Witayanont <reef_3llen@hotmail.com>
2022-05-25 14:34:17 +0700

    add ocr

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import uvicorn
 import numpy as np
 import tensorflow as tf
 
+from fn import get_ocr_locations
 from PIL import Image
 from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
@@ -13,71 +14,77 @@ from linebot.models import MessageEvent, TextMessage, ImageMessage, TextSendMess
 
 app = FastAPI()
 
-line_bot_api = LineBotApi(os.getenv('LINE_CHANNEL_ACCESS_TOKEN'))
-handler = WebhookHandler(os.getenv('LINE_CHANNEL_SECRET'))
-s3_public = os.getenv('AWS_S3_PUBLIC_PATH')
+line_bot_api = LineBotApi(os.getenv("LINE_CHANNEL_ACCESS_TOKEN"))
+handler = WebhookHandler(os.getenv("LINE_CHANNEL_SECRET"))
 
-classification_model = tf.keras.models.load_model('./models/bank_classification_model.h5')
-classification_labels = pickle.loads(open('./models/bank_classification_labels.pickle', 'rb').read())
+classification_model = tf.keras.models.load_model("./models/bank_classification_model.h5")
+classification_labels = pickle.loads(open("./models/bank_classification_labels.pickle", "rb").read())
 
 
-@app.get('/')
+@app.get("/")
 def index():
-    return {'line': os.getenv('LINE_BOT_BASIC_ID')}
+    return {"line": os.getenv("LINE_BOT_BASIC_ID")}
 
 
-@app.post('/webhook')
+@app.post("/webhook")
 async def callback(request: Request, x_line_signature: str = Header(None)):
     body = await request.body()
     try:
         handler.handle(body.decode("utf-8"), x_line_signature)
     except InvalidSignatureError:
         raise HTTPException(status_code=400, detail="chatbot handle body error.")
-    return 'OK'
+    return "OK"
 
 
 @handler.add(MessageEvent, message=TextMessage)
 def message_text(event):
-    print('text-event', event)
+    print("text-event", event)
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text='กรุณาอัพโหลดรูป')
+        TextSendMessage(text="กรุณาอัพโหลดรูป")
     )
 
 
 @handler.add(MessageEvent, message=ImageMessage)
 def message_text(event):
-    print('image-event', event)
+    print("image-event", event)
 
     # save image to file
     message_content = line_bot_api.get_message_content(event.message.id)
-    with open('./image.png', 'wb') as fd:
+    with open("./image.png", "wb") as fd:
         for chunk in message_content.iter_content():
             fd.write(chunk)
 
     # load image
-    img = Image.open('./image.png').resize((180, 180))
+    img = Image.open("./image.png").resize((180, 180))
     img = np.array(img)
 
     # predict bank
     pred = classification_model.predict(img[None, :, :])
     pred_prob = tf.nn.softmax(pred).numpy()
 
-    messages = []
-    messages.append('bank = {}'.format(classification_labels[np.argmax(pred_prob)]))
-    messages.append('probability = {}'.format(np.max(pred_prob)))
+    bank_class = classification_labels[np.argmax(pred_prob)] if np.max(pred_prob) > 0.21 else "OTHER"
+    if bank_class == "OTHER":
+        msg = TextSendMessage(text="กรุณาอัพโหลดรูปสลิป")
+        line_bot_api.reply_message(event.reply_token, msg)
+    else:
+        ocr_locations = get_ocr_locations(bank_class)
+        messages = ""
 
-    raw = {}
-    for c in range(len(classification_labels)):
-        raw[classification_labels[c]] = pred_prob[0][c]
-    messages.append('raw = {}'.format(raw))
+        for i in range(len(ocr_locations)):
+            (x, y, w, h) = ocr_locations[i].bbox
 
-    for i in range(len(messages)):
-        msg = TextSendMessage(text=messages[i])
-        if i == 0:
-            line_bot_api.reply_message(event.reply_token, msg)
-        else:
-            line_bot_api.push_message(event.source.user_id, msg)
+            roi = img[y:y+h, x:x+w]
+            text = pytesseract.image_to_string(roi, lang="tha+eng")
+
+            messages += "{} = {}\n".format(str(ocr_locations[i].id), text.strip())
+
+        for i in range(len(messages)):
+            msg = TextSendMessage(text=messages[i])
+            if i == 0:
+                line_bot_api.reply_message(event.reply_token, msg)
+            else:
+                line_bot_api.push_message(event.source.user_id, msg)
 
 
 if __name__ == "__main__":
---
+++ fn.py
@@ -0,0 +1,64 @@
+from collections import namedtuple
+
+def get_ocr_locations(bank):
+    OCRLocation = namedtuple('OCRLocation', ['id', 'bbox'])
+    locations = {
+        "BAY": [
+            OCRLocation('from', (500, 310, 510, 60)),
+            OCRLocation('to', (500, 525, 510, 60)),
+            OCRLocation('amount', (500, 705, 400, 60)),
+            OCRLocation('date', (320, 165, 205, 40)),
+            OCRLocation('time', (525, 165, 145, 40)),
+            OCRLocation('ref', (75, 960, 305, 60))
+        ],
+        "BBL": [
+            OCRLocation('from', (400, 1340, 400, 50)),
+            OCRLocation('to', (400, 1580, 500, 50)),
+            OCRLocation('amount', (400, 1100, 320, 90)),
+            OCRLocation('date', (350, 940, 220, 50)),
+            OCRLocation('time', (580, 940, 150, 50)),
+            OCRLocation('ref', (70, 2370, 650, 50))
+        ],
+        "GOV": [
+            OCRLocation('from', (400, 1340, 400, 50)),
+            OCRLocation('to', (400, 1580, 500, 50)),
+            OCRLocation('amount', (400, 1100, 320, 90)),
+            OCRLocation('date', (350, 940, 220, 50)),
+            OCRLocation('time', (580, 940, 150, 50)),
+            OCRLocation('ref', (70, 2370, 650, 50))
+        ],
+        "KBANK": [
+            OCRLocation('from', (220, 240, 420, 50)),
+            OCRLocation('to', (220, 550, 420, 50)),
+            OCRLocation('amount', (210, 985, 320, 50)),
+            OCRLocation('date', (60, 100, 195, 50)),
+            OCRLocation('time', (255, 100, 160, 50)),
+            OCRLocation('ref', (220, 865, 400, 50))
+        ],
+        "KTB": [
+            OCRLocation('from', (180, 480, 500, 50)),
+            OCRLocation('to', (180, 680, 500, 50)),
+            OCRLocation('amount', (560, 900, 320, 50)),
+            OCRLocation('date', (600, 1050, 230, 50)),
+            OCRLocation('time', (865, 1050, 100, 50)),
+            OCRLocation('ref', (330, 360, 350, 50))
+        ],
+        "SCB": [
+            OCRLocation('from', (350, 670, 700, 70)),
+            OCRLocation('to', (350, 880, 700, 70)),
+            OCRLocation('amount', (650, 1320, 370, 70)),
+            OCRLocation('date', (340, 415, 250, 70)),
+            OCRLocation('time', (620, 415, 120, 70)),
+            OCRLocation('ref', (355, 490, 565, 70))
+        ],
+        "TMB": [
+            OCRLocation('from', (500, 1280, 935, 70)),
+            OCRLocation('to', (500, 1665, 935, 100)),
+            OCRLocation('amount', (550, 740, 320, 120)),
+            OCRLocation('date', (950, 1880, 270, 90)),
+            OCRLocation('time', (1220, 1880, 215, 90)),
+            OCRLocation('ref', (900, 2070, 535, 90))
+        ]
+    }
+
+    return locations[bank]
