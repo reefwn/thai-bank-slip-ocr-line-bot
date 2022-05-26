8c6dd2e62adf787bde8a6adf762f2d8e8053c01d
Yossapol Witayanont <reef_3llen@hotmail.com>
2022-05-26 23:00:44 +0700

    using index from boxing ocr for gov

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ import tensorflow as tf
 
 from PIL import Image
 from linebot import LineBotApi, WebhookHandler
-from fn import get_img_size, get_ocr_locations
+from fn import get_img_size, get_ocr_locations, get_rois, gov_ocr
 from linebot.exceptions import InvalidSignatureError
 from fastapi import FastAPI, Request, Header, HTTPException
 from linebot.models import MessageEvent, TextMessage, ImageMessage, TextSendMessage
@@ -63,10 +63,10 @@ def message_text(event):
 
     # load image for classification
     img = Image.open(IMG_FILE_NAME).resize((180, 180))
-    img = np.array(img)
+    img_arr = np.array(img)
 
     # predict bank
-    pred = classification_model.predict(img[None, :, :])
+    pred = classification_model.predict(img_arr[None, :, :])
     pred_prob = tf.nn.softmax(pred).numpy()
 
     # find bank class name and probability
@@ -88,29 +88,34 @@ def message_text(event):
         img_size = get_img_size(bank_class)
         img = cv2.imread(IMG_FILE_NAME)
         [width, height, _] = img.shape
+        
+        if bank_class == "GOV":
+            rois = get_rois(img)
+            rdtfta = ["ref", "date", "time", "from", "to", "amount"]
+            ocr = gov_ocr(rois)
+            for i in range(len(rdtfta)):
+                messages.append("{}: {}".format(rdtfta[i], ocr[i]))
+        else:
+            # get locations for ocr
+            ocr_locations = get_ocr_locations(bank_class)
 
-        print("img shape: {}".format(img.shape))
+            # ocr on each box
+            try:
+                for i in range(len(ocr_locations)):
+                    (x, y, w, h) = ocr_locations[i].bbox
 
-        # get locations for ocr
-        ocr_locations = get_ocr_locations(bank_class)
+                    if bank_class == "GOV":
+                        x = int(width * x)
+                        y = int(height * y)
 
-        # ocr on each box
-        try:
-            for i in range(len(ocr_locations)):
-                (x, y, w, h) = ocr_locations[i].bbox
+                    roi = img[y:y+h, x:x+w]
 
-                if bank_class == "GOV":
-                    x = int(width * x)
-                    y = int(height * y)
+                    lang = "eng" if i == len(ocr_locations) - 1 else "tha+eng"
+                    text = pytesseract.image_to_string(roi, lang=lang)
 
-                roi = img[y:y+h, x:x+w]
-
-                lang = "eng" if i == len(ocr_locations) - 1 else "tha+eng"
-                text = pytesseract.image_to_string(roi, lang=lang)
-
-                messages.append("{}: {}".format(str(ocr_locations[i].id), text.strip()))
-        except:
-            print("something went wrong on ocr")
+                    messages.append("{}: {}".format(str(ocr_locations[i].id), text.strip()))
+            except:
+                print("something went wrong on ocr")
 
         combine_msgs = "\n".join(messages)
         msg = TextSendMessage(text=combine_msgs)
--- fn.py
+++ fn.py
@@ -1,4 +1,11 @@
+import pytesseract
+import re
+
 from collections import namedtuple
+from pytesseract import Output
+
+SPECIAL_CHARACTERS = "!@#$%^&*()-+?_=,<>/"
+
 
 def get_img_size(bank):
     sizes = {
@@ -15,64 +22,131 @@ def get_img_size(bank):
 
 
 def get_ocr_locations(bank):
-    OCRLocation = namedtuple('OCRLocation', ['id', 'bbox'])
+    OCRLocation = namedtuple("OCRLocation", ["id", "bbox"])
     locations = {
         "BAY": [
-            OCRLocation('from', (500, 310, 510, 60)),
-            OCRLocation('to', (500, 525, 510, 60)),
-            OCRLocation('amount', (500, 705, 400, 60)),
-            OCRLocation('date', (320, 165, 205, 40)),
-            OCRLocation('time', (525, 165, 145, 40)),
-            OCRLocation('ref', (75, 960, 305, 60))
+            OCRLocation("from", (500, 310, 510, 60)),
+            OCRLocation("to", (500, 525, 510, 60)),
+            OCRLocation("amount", (500, 705, 400, 60)),
+            OCRLocation("date", (320, 165, 205, 40)),
+            OCRLocation("time", (525, 165, 145, 40)),
+            OCRLocation("ref", (75, 960, 305, 60))
         ],
         "BBL": [
-            OCRLocation('from', (400, 1340, 400, 50)),
-            OCRLocation('to', (400, 1580, 500, 50)),
-            OCRLocation('amount', (400, 1100, 320, 90)),
-            OCRLocation('date', (350, 940, 220, 50)),
-            OCRLocation('time', (580, 940, 150, 50)),
-            OCRLocation('ref', (70, 2370, 650, 50))
+            OCRLocation("from", (400, 1340, 400, 50)),
+            OCRLocation("to", (400, 1580, 500, 50)),
+            OCRLocation("amount", (400, 1100, 320, 90)),
+            OCRLocation("date", (350, 940, 220, 50)),
+            OCRLocation("time", (580, 940, 150, 50)),
+            OCRLocation("ref", (70, 2370, 650, 50))
         ],
         "GOV": [
-            OCRLocation('from', (400, 1340, 400, 50)),
-            OCRLocation('to', (400, 1580, 500, 50)),
-            OCRLocation('amount', (400, 1100, 320, 90)),
-            OCRLocation('date', (350, 940, 220, 50)),
-            OCRLocation('time', (580, 940, 150, 50)),
-            OCRLocation('ref', (70, 2370, 650, 50))
+            OCRLocation("from", (400, 1340, 400, 50)),
+            OCRLocation("to", (400, 1580, 500, 50)),
+            OCRLocation("amount", (400, 1100, 320, 90)),
+            OCRLocation("date", (350, 940, 220, 50)),
+            OCRLocation("time", (580, 940, 150, 50)),
+            OCRLocation("ref", (70, 2370, 650, 50))
         ],
         "KBANK": [
-            OCRLocation('from', (220, 240, 420, 50)),
-            OCRLocation('to', (220, 550, 420, 50)),
-            OCRLocation('amount', (210, 985, 320, 50)),
-            OCRLocation('date', (60, 100, 195, 50)),
-            OCRLocation('time', (255, 100, 160, 50)),
-            OCRLocation('ref', (220, 865, 400, 50))
+            OCRLocation("from", (220, 240, 420, 50)),
+            OCRLocation("to", (220, 550, 420, 50)),
+            OCRLocation("amount", (210, 985, 320, 50)),
+            OCRLocation("date", (60, 100, 195, 50)),
+            OCRLocation("time", (255, 100, 160, 50)),
+            OCRLocation("ref", (220, 865, 400, 50))
         ],
         "KTB": [
-            OCRLocation('from', (180, 480, 500, 50)),
-            OCRLocation('to', (180, 680, 500, 50)),
-            OCRLocation('amount', (560, 900, 320, 50)),
-            OCRLocation('date', (600, 1050, 230, 50)),
-            OCRLocation('time', (865, 1050, 100, 50)),
-            OCRLocation('ref', (330, 360, 350, 50))
+            OCRLocation("from", (180, 480, 500, 50)),
+            OCRLocation("to", (180, 680, 500, 50)),
+            OCRLocation("amount", (560, 900, 320, 50)),
+            OCRLocation("date", (600, 1050, 230, 50)),
+            OCRLocation("time", (865, 1050, 100, 50)),
+            OCRLocation("ref", (330, 360, 350, 50))
         ],
         "SCB": [
-            OCRLocation('from', (350, 670, 700, 70)),
-            OCRLocation('to', (350, 880, 700, 70)),
-            OCRLocation('amount', (650, 1320, 370, 70)),
-            OCRLocation('date', (340, 415, 250, 70)),
-            OCRLocation('time', (620, 415, 120, 70)),
-            OCRLocation('ref', (355, 490, 565, 70))
+            OCRLocation("from", (350, 670, 700, 70)),
+            OCRLocation("to", (350, 880, 700, 70)),
+            OCRLocation("amount", (650, 1320, 370, 70)),
+            OCRLocation("date", (340, 415, 250, 70)),
+            OCRLocation("time", (620, 415, 120, 70)),
+            OCRLocation("ref", (355, 490, 565, 70))
         ],
         "TMB": [
-            OCRLocation('from', (500, 1280, 935, 70)),
-            OCRLocation('to', (500, 1665, 935, 100)),
-            OCRLocation('amount', (550, 740, 320, 120)),
-            OCRLocation('date', (950, 1880, 270, 90)),
-            OCRLocation('time', (1220, 1880, 215, 90)),
-            OCRLocation('ref', (900, 2070, 535, 90))
+            OCRLocation("from", (500, 1280, 935, 70)),
+            OCRLocation("to", (500, 1665, 935, 100)),
+            OCRLocation("amount", (550, 740, 320, 120)),
+            OCRLocation("date", (950, 1880, 270, 90)),
+            OCRLocation("time", (1220, 1880, 215, 90)),
+            OCRLocation("ref", (900, 2070, 535, 90))
         ]
     }
 
     return locations[bank]
+
+
+def get_rois(img):
+    d = pytesseract.image_to_data(img, output_type=Output.DICT)
+    n_boxes = len(d["level"])
+
+    duplicates = []
+    count = 0
+    rois = []
+
+    p = 10
+
+    for i in range(n_boxes):
+        (x, y, w, h) = (d["left"][i], d["top"][i], d["width"][i], d["height"][i])
+
+        if x != 0 and y != 0 and w != img.shape[0] and h != img.shape[1] and "{}{}".format(w, h) not in duplicates:
+            duplicates.append("{}{}".format(w, h))
+            count += 1
+            roi = img[y-p:y+h+p, x-p:x+w+p]
+            rois.append(roi)
+
+    return rois
+
+
+def gov_ocr(rois):
+    ref = ""
+    date = ""
+    time = ""
+    from_ = ""
+    to = ""
+    amount = 0
+
+    for i in range(len(rois)):
+        # ref
+        if i == 6:
+            txt = pytesseract.image_to_string(rois[i], lang="eng")
+            if any(c in SPECIAL_CHARACTERS for c in txt):
+                refs = txt.split(" ")
+            for r in refs:
+                if not any(c in SPECIAL_CHARACTERS for c in r):
+                    ref = r.strip()
+            else:
+                ref = txt.strip()
+        # date time
+        if i == 7:
+            text = pytesseract.image_to_string(rois[i], lang="tha+eng")
+            datetime = text.split(" ")
+            date = " ".join(datetime[:3]).strip()
+            time = " ".join(datetime[3:]).strip()
+        # from
+        if i == 14:
+            txt = pytesseract.image_to_string(rois[i], lang="tha+eng")
+            t = txt.split("\n")
+            from_ = t[0]
+        # to
+        if i == 22 or i == 24:
+            txt = pytesseract.image_to_string(rois[i], lang="tha+eng")
+            if to == "" and not any(c in SPECIAL_CHARACTERS for c in text):
+                to = txt.strip()
+        # amount
+        if i == len(rois) - 2 or i == len(rois) - 1:
+            txt = pytesseract.image_to_string(rois[i], lang="tha+eng")
+            txt_int = re.findall(r"[0-9]+", txt)
+            if len(txt_int) > 0:
+                amount = int(txt_int[0]) if int(amount) == 0 else amount
+
+    return [ref, date, time, from_, to, amount]
